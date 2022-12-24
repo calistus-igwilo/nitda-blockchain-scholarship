a44608062ea269703ce333b105436ebbc014a814
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-24 09:10:35 +0100

    add files

--- javascript/testing.js
+++ javascript/testing.js
@@ -1,3 +1,36 @@
-for (var i = 0; i < 10; i++) {
+/* for (var i = 0; i < 10; i++) {
     console.log(i);
+} */
+
+/* function nitdaAccepted() {
+    console.log("Congrats, you have been accepted into NBS");
+    console.log("Enroll to Bitcoin Theory course at 5thwork.com");
 }
+
+nitdaAccepted(); */
+
+/* ar numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function evenNumbers(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+evenNumbers(numArray); */
+
+/* function sumNum(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+console.log(sumNum(5, 7)); */
+
+var a = ["a", "b", "c", "d"];
+var i = 0;
+while (a[i]) {
+    i++;
+}
+console.log(i);
